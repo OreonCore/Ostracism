9c2bd839f7829fed7e39e2ad55072e5ce2f78f3c
OreonCore <114487010+OreonCore@users.noreply.github.com>
2025-03-12 10:17:04 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,28 @@
+function removeRuResults() {
+    document.querySelectorAll('div.g, div.MjjYud').forEach(result => {
+        let link = result.querySelector('a')?.href;
+        let textContent = result.innerText || "";
+
+        if (!link) return;
+
+        let blockedPatterns = [
+	    /ru/,
+	    /\.ru\b/,
+	    /\.su\b/,
+            /\/ru\//,
+	    /\/ru-ru\//,
+            /\/ru-RU\//,
+            /ru\./,
+	    /\/ru-[a-z]{2}\//,
+            /[\?&]lang=ru\b/, /[\?&]lang=ru-[a-z]{2}\b/,
+            /yandex\./, /tass\./, /rt\./, /sputniknews\./, /vk\./
+        ];
+
+        if (blockedPatterns.some(pattern => pattern.test(link))) {
+            result.style.display = 'none';
+        }
+    });
+}
+
+removeRuResults();
+setInterval(removeRuResults, 3000);

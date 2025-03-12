fa1a895da81f3913222e0937b4eb113a0e385ad5
OreonCore <114487010+OreonCore@users.noreply.github.com>
2025-03-12 10:49:52 +0200

    Update content.js
    Прибрав один клас, оскільки є підозра що через нього сторінка картинок ломається

--- content.js
+++ content.js
@@ -1,28 +1,27 @@
 function removeRuResults() {
-    document.querySelectorAll('div.g, div.MjjYud').forEach(result => {
+    document.querySelectorAll('div.g').forEach(result => {
         let link = result.querySelector('a')?.href;
-        let textContent = result.innerText || "";
 
         if (!link) return;
 
         let blockedPatterns = [
-	    /ru/,
-	    /\.ru\b/,
-	    /\.su\b/,
+            /ru/,
+            /\.ru\b/,
+            /\.su\b/,
             /\/ru\//,
-	    /\/ru-ru\//,
+            /\/ru-ru\//,
             /\/ru-RU\//,
             /ru\./,
-	    /\/ru-[a-z]{2}\//,
+            /\/ru-[a-z]{2}\//,
             /[\?&]lang=ru\b/, /[\?&]lang=ru-[a-z]{2}\b/,
             /yandex\./, /tass\./, /rt\./, /sputniknews\./, /vk\./
         ];
 
         if (blockedPatterns.some(pattern => pattern.test(link))) {
-            result.style.display = 'none';
+            result.style.display = 'none'; 
         }
     });
 }
 
 removeRuResults();
-setInterval(removeRuResults, 3000);
+setInterval(removeRuResults, 3000); 
